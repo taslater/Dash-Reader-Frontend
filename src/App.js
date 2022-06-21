746f3e37c85e2bc42416f482ba82a763998a90d5
Thomas Slater <thomas.a.slater@gmail.com>
2022-06-21 14:26:34 -0400

    header nav (hamburger), sizing, footer

--- src/App.js
+++ src/App.js
@@ -43,8 +43,26 @@ function App() {
   ]
   return (
     <div className="App">
-      <h1 className="header">--Dash Reader--</h1>
+      <header className="header">
+        <h1 className='title'>--Dash Reader--</h1>
+        <div class="pos-f-t">
+          <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4">
+              <h5 class="text-white h4">Collapsed content</h5>
+              <span class="text-muted">Toggleable via the navbar brand.</span>
+            </div>
+          </div>
+          <nav class="navbar navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </nav>
+        </div>
+      </header>
       <UIContainer reader_uis={reader_uis}/>
+      <footer>
+        <p>Check out our <a href="https://readgood.azurewebsites.net/">API</a></p>
+      </footer>
     </div>
   );
 }
