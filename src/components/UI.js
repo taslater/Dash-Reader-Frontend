358e20f36fe13c1580f6357ac293493493962625
Thomas Slater <thomas.a.slater@gmail.com>
2022-06-21 21:22:28 -0400

    sticky menu is dropdown

--- src/components/UI.js
+++ src/components/UI.js
@@ -5,13 +5,21 @@ function UI(props) {
   for (const i in props.buttonNames) {
     const buttonName = props.buttonNames[i]
     optionButtons.push(
-      <button key={i} className="optionButton">{buttonName}</button>
+      <button key={i} className="dropdown-item">{buttonName}</button>
     )
   }
   return (
     <section className='uisection'>
       {props.children}
-      <section className='optionButtons'>{optionButtons}</section>
+      {/* <section className='optionButtons'>{optionButtons}</section> */}
+      <div class="dropdown">
+        <button class="btn dropdown-toggle optionButtons" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Options Menu
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          {optionButtons}
+        </div>
+      </div>
     </section>
   )
 }
