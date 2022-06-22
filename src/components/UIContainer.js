22995a000b0af16d7767a0360bc672e8be2c11fb
Thomas Slater <thomas.a.slater@gmail.com>
2022-06-22 14:29:31 -0400

    separate MediaControl component

--- src/components/UIContainer.js
+++ src/components/UIContainer.js
@@ -1,9 +1,8 @@
 import './UIContainer.css'
-import reset_icon from '../static/icons/arrow-clockwise.svg'
-import play_icon from '../static/icons/play-circle.svg'
-import pause_icon from '../static/icons/pause-circle.svg'
 import UI from './UI';
+import MediaControl from './MediaControl';
 import React, { useState } from 'react'
+
 
 function UIContainer() {
 
@@ -46,17 +45,7 @@ function UIContainer() {
       ]}>
       <div id="rsvp-div" className="textarea">
         {/* {inputText} */}
-        <div className="rsvp-icons">
-          <button className="rsvp-control-button">
-            <img src={reset_icon} alt="Reset" width="36" height="36" className="rsvp-icon"></img>
-          </button>
-          <button className="rsvp-control-button">
-            <img src={play_icon} alt="Play" width="32" height="32" className="rsvp-icon"></img>
-          </button>
-          <button className="rsvp-control-button">
-            <img src={pause_icon} alt="Pause" width="32" height="32" className="rsvp-icon"></img>
-          </button>
-        </div>
+        <MediaControl />
       </div>
     </UI>,
     <UI
